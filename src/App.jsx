b32a9c188a8d833d5be134e418127ee16e846ec5
Flavio Oliveira <61277545+fl4viooliveira@users.noreply.github.com>
2023-03-04 20:02:05 +0000

    Add deploy gh using Hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <Router basename="/">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route exact path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }

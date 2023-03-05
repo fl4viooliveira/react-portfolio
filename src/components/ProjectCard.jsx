46e8cee7c0734237ca379eaf0d99b08c18409ba5
Flavio Oliveira <61277545+fl4viooliveira@users.noreply.github.com>
2023-03-05 18:01:14 +0000

    Add card style

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,7 +1,9 @@
 
+import styles from "../styles/ProjectCard.module.css"
+
 export default function ProjectCard(props) {
   return (
-  <div className="card card-custom" style={{"width": "18rem"}}>
+  <div className={`card ${styles.cardCustom}`} style={{width: "18rem",}}>
       <a href={props.deployed} target="_blank" rel="noreferrer">
         <img
           src={props.img}
@@ -10,7 +12,7 @@ export default function ProjectCard(props) {
           alt={props.name}
         />
         <div className="card-body">
-          <h5 className="card-title text-center">{props.name}</h5>
+          <h5 className="card-title text-center text-light">{props.name}</h5>
         </div>
       </a>
     </div>

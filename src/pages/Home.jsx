4499a2f70aa1356718c3d0be7ab29e81810582be
Flavio Oliveira <61277545+fl4viooliveira@users.noreply.github.com>
2023-03-05 18:09:53 +0000

    Add skills on Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,67 @@
-import Layout from "../components/Layout"
+import Layout from "../components/Layout";
 export default function Home() {
   return (
     <Layout>
-    <div>
-      <h1>Home</h1>
-      <h2>Test12341</h2>
-      <h3>ssssssssdfsdfasdfasdfasd</h3>
-    </div>
-
+      <section class="container-fluid skill-box" id="skills">
+        <h1 class="text-center my-5">Skills</h1>
+        <div class="jumbotron skills-jumbo jumbotron-fluid">
+          <h3 class="mx-3">Git / GitHub</h3>
+          <div class="progress m-3">
+            <div
+              class="progress-bar progress-bar-striped progress-bar-animated"
+              role="progressbar"
+              style={{width: "85%"}}
+              aria-valuenow="10"
+              aria-valuemin="0"
+              aria-valuemax="100"
+            ></div>
+          </div>
+          <h3 class="mx-3">HTML / CSS</h3>
+          <div class="progress m-3">
+            <div
+              class="progress-bar progress-bar-striped bg-success progress-bar-animated"
+              role="progressbar"
+              style={{width: "95%"}}
+              aria-valuenow="25"
+              aria-valuemin="0"
+              aria-valuemax="100"
+            ></div>
+          </div>
+          <h3 class="mx-3">JavaScript</h3>
+          <div class="progress m-3">
+            <div
+              class="progress-bar progress-bar-striped progress-bar-animated progress-bar-animated bg-info"
+              role="progressbar"
+              style={{width: "85%"}}
+              aria-valuenow="50"
+              aria-valuemin="0"
+              aria-valuemax="100"
+            ></div>
+          </div>
+          <h3 class="mx-3">Work with APIs</h3>
+          <div class="progress m-3">
+            <div
+              class="progress-bar progress-bar-striped progress-bar-animated bg-warning"
+              role="progressbar"
+              style={{width: "80%"}}
+              aria-valuenow="75"
+              aria-valuemin="0"
+              aria-valuemax="100"
+            ></div>
+          </div>
+          <h3 class="mx-3">React</h3>
+          <div class="progress m-3">
+            <div
+              class="progress-bar progress-bar-striped progress-bar-animated bg-danger"
+              role="progressbar"
+              style={{width: "95%"}}
+              aria-valuenow="100"
+              aria-valuemin="0"
+              aria-valuemax="100"
+            ></div>
+          </div>
+        </div>
+      </section>
     </Layout>
   );
 }

ca44c27adea0cc6b5b258d40525f9378b5f38190
Flavio Oliveira <61277545+fl4viooliveira@users.noreply.github.com>
2023-03-05 12:32:22 +0000

    Update navbar

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,7 +3,7 @@ import Header from "./Header";
 
 export default function Layout(props) {
   return (
-    <div>
+    <div className="container">
       <Header />
       {props.children}
       <Footer />
